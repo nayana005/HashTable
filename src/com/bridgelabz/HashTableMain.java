8e5209cb81733d41084f36db42d9060d9b013e91
nayana005 <nayanasuresh1005@gmail.com>
2022-09-02 16:51:40 +0530

    Added: Remove avoidable word from sentence

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -2,21 +2,25 @@ package com.bridgelabz;
 
 public class HashTableMain {
 
-	public static void main(String[] args) {
-		String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations" ;
-		String[] splitArray = paragraph.split(" ");
-		MyHashTable<String,Integer> hashTable = new MyHashTable<>();
-		for (String word:splitArray) {
-			Integer value = hashTable.get(word);
-			if(value ==  null){
-				value = 1;
-			}
-			else
-				value = value+1;
-			hashTable.add(word,value);
-		}
-		hashTable.print();
-		System.out.println();
-	}
+	 public static void main(String[] args) {
+	        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations" ;
+	        String[] splitArray = paragraph.split(" ");
+	        MyHashTable<String,Integer> hashTable = new MyHashTable<>();
+	        for (String word:splitArray) {
+	            Integer value = hashTable.get(word);
+	            if(value ==  null){
+	                value = 1;
+	            }
+	            else
+	                value = value+1;
+	            hashTable.add(word,value);
+	        }
+	        hashTable.print();
+	        System.out.println();
+	        System.out.println("***********************************");
+
+	        hashTable.remove("avoidable");
+	        hashTable.print();
+	    }
 
 }
--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -2,43 +2,54 @@ package com.bridgelabz;
 
 public class LinkedList<K,V> {
     
-	 MyMapNode<K, V> head;
-	    MyMapNode<K, V> tail;
+	MyMapNode<K, V> head;
+    MyMapNode<K, V> tail;
 
-	 public MyMapNode<K,V> search(K searchData) {
-	        MyMapNode<K,V> temp = head;
-	        while(temp != null){
-	            if(temp.key.equals(searchData))
-	                return temp;
-	            temp = temp.next;
-	        }
-	        return null;
-	    }
+ public MyMapNode<K,V> search(K searchData) {
+        MyMapNode<K,V> temp = head;
+        while(temp != null){
+            if(temp.key.equals(searchData))
+                return temp;
+            temp = temp.next;
+        }
+        return null;
+    }
 
-	  public void append(MyMapNode<K,V> node ) {
-	        if(head == null) {
-	            head = node;
-	            tail = node;
-	        }
-	        else{
-	            tail.next = node;
-	            tail = node;
-	           }
-	       }
 
-	   public void print() {
-	       MyMapNode<K,V> temp = head;
-	        while(temp != null){
-	            System.out.println(temp);
-	            temp = temp.next;
-	        }
-	    }
+  public void append(MyMapNode<K,V> node ) {
 
-	    @Override
-	    public String toString() {
-	        return "LinkedList{" +
-	                "head=" + head +
-	                ", tail=" + tail +
-	                '}';
-	  }
+        if(head == null) {
+            head = node;
+            tail = node;
+        }
+        else{
+            tail.next = node;
+            tail = node;
+           }
+       }
+
+   public void print() {
+       MyMapNode<K,V> temp = head;
+        while(temp != null){
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
+
+    public void delete(K key){
+        MyMapNode<K,V> temp = head;
+        MyMapNode<K,V> searchedElement = search(key);
+        while(temp.next != searchedElement){
+            temp = temp.next;
+        }
+        temp.next = searchedElement.next;
+    }
+
+    @Override
+    public String toString() {
+        return "LinkedList{" +
+                "head=" + head +
+                ", tail=" + tail +
+                '}';
+    }
 }
